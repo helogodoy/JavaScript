a2a2ee129cc08f8c7be02e6233953d565ed612db
Maria Heloísa de Godoy <godoymariaheloisa@gmail.com>
2024-04-01 10:27:37 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>JavaScript Hello Word!</title> 
+
+    <script>
+        function main() {
+            alert("Hello Word!"); 
+        }
+    </script>
+</head>
+<body/>
+
+</body>
+</html>
+
+
